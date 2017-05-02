f056a32e1f38d776db723115da4c4906f0c5e5da
Alimjan <maninwindow@hotmail.com>
2017-05-02 23:20:07 +0800

    Initialize the project
    Initialize the project

---
+++ FirstMVCApplicatio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstMVCApplicatio.Startup))]
+namespace FirstMVCApplicatio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
